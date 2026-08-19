3109acaaab4568b7463642c42b9f05f01e8ccdf9
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 17:11:54 +0530

    refactor: remove unused import for DBRef in UrlUser entity

--- src/main/java/com/spring/springboot/UrlShortener/entity/UrlUser.java
+++ src/main/java/com/spring/springboot/UrlShortener/entity/UrlUser.java
@@ -9,7 +9,6 @@ import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
