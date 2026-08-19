6ea883e0fd53b781e57d9701efb1d9c5d1b573a7
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 17:09:42 +0530

    refactor: remove Swagger annotations from OtpRequestDto, ReportLinkRequestDto, and SignupRequestDto

--- src/main/java/com/spring/springboot/UrlShortener/dto/requestDtos/OtpRequestDto.java
+++ src/main/java/com/spring/springboot/UrlShortener/dto/requestDtos/OtpRequestDto.java
@@ -1,6 +1,5 @@
 package com.spring.springboot.UrlShortener.dto.requestDtos;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -13,24 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Schema(
-        description = "Request payload used to generate an OTP for email verification"
-)
 public class OtpRequestDto {
 
-    @Schema(
-            description = "Email address to which the OTP will be sent",
-            example = "user@example.com",
-            requiredMode = Schema.RequiredMode.REQUIRED
-    )
     @Email(message = "Enter a valid email id.")
     private String entityEmail;
 
-    @Schema(
-            description = "Name of the entity requesting OTP (user or reporter)",
-            example = "Vishal",
-            requiredMode = Schema.RequiredMode.REQUIRED
-    )
     @NotNull
     @NotBlank
     private String entityName;
