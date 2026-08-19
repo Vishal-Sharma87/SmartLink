be2b4bf5f10d13195efdf4a558d700450dc8ea57
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 18:33:12 +0530

    feat: refactor AnalysisService to integrate IPinfoLite for IP geolocation and streamline click count logic

--- src/main/java/com/spring/springboot/UrlShortener/services/AnalysisService.java
+++ src/main/java/com/spring/springboot/UrlShortener/services/AnalysisService.java
@@ -1,6 +1,5 @@
 package com.spring.springboot.UrlShortener.services;
 
-
 import com.spring.springboot.UrlShortener.dto.TrackPayloadDto;
 import com.spring.springboot.UrlShortener.dto.geoIpResponses.IpInfo;
 import com.spring.springboot.UrlShortener.entity.LinkInformation;
@@ -9,28 +8,101 @@ import com.spring.springboot.UrlShortener.enums.Browser;
 import com.spring.springboot.UrlShortener.enums.Device;
 import com.spring.springboot.UrlShortener.enums.OperatingSystem;
 import com.spring.springboot.UrlShortener.model.LinkAnalysisDto;
-import com.spring.springboot.UrlShortener.services.links.LinkService;
+import io.ipinfo.api.IPinfoLite;
+import io.ipinfo.api.errors.RateLimitedException;
+import io.ipinfo.api.model.IPResponseLite;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.time.Instant;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class AnalysisService {
 
-    private static final String GENERIC_IP_INFO_API = "ipapi.co/{ip}/json";
     private final KafkaTemplate<String, LinkAnalysisDto> kafkaTemplate;
-    private final WebClient webClient;
     private final LinkService linkService;
     private final LinkInformationService linkInformationService;
+
+    private final IPinfoLite client;
+
+    public AnalysisService(
+            @Value("${ipinfo.token}") String ipInfoToken,
+            KafkaTemplate<String, LinkAnalysisDto> kafkaTemplate,
+            LinkService linkService,
+            LinkInformationService linkInformationService) {
+
+        this.kafkaTemplate = kafkaTemplate;
+        this.linkService = linkService;
+        this.linkInformationService = linkInformationService;
+        this.client = new IPinfoLite.Builder()
+                .setToken(ipInfoToken)
+                .build();
+
+    }
+
+    public void sendDataToKafka(TrackPayloadDto dto, HttpServletRequest request) {
+
+        String ip = getClientsIp(request);
+
+        LinkAnalysisDto linkAnalysisDto = LinkAnalysisDto.builder()
+                .entityIp(ip)
+                .trackPayloadDto(dto)
+                .build();
+        kafkaTemplate.send("URL_SHORTENER_existed_link_analysis", linkAnalysisDto);
+    }
+
+    public void analyze(LinkAnalysisDto linkAnalysisDto) {
+
+        String hash = linkAnalysisDto.getTrackPayloadDto().getShortHash();
+        Links linkInDb = linkService.getLinkByHash(hash);
+
+        if (hash == null || hash.isBlank() || linkInDb == null)
+            return;
+
+        String ip = linkAnalysisDto.getEntityIp();
+        IpInfo ipInfo = getIpRelatedGeoInfoResponse(ip);
+
+        int width;
+        if (linkAnalysisDto.getTrackPayloadDto().getViewportWidth() == null
+                && linkAnalysisDto.getTrackPayloadDto().getScreenWidth() == null) {
+            width = 0;
+        } else {
+            width = linkAnalysisDto.getTrackPayloadDto().getViewportWidth() == null
+                    ? linkAnalysisDto.getTrackPayloadDto().getScreenWidth()
+                    : linkAnalysisDto.getTrackPayloadDto().getViewportWidth();
+        }
+
+        Browser browser = categoriseBrowserBasedOnUserAgent(linkAnalysisDto.getTrackPayloadDto().getUserAgent());
+
+        OperatingSystem os = categoriseOperatingSystemBasedOnUserAgent(
+                linkAnalysisDto.getTrackPayloadDto().getUserAgent());
+
+        Device device = categoriseDeviceBasedOnWidthAndOperatingSystem(width, os);
+
+        linkInDb.incrementClickCount();
+        linkService.save(linkInDb);
+
+        LinkInformation.ClickerDeviceInfo deviceInfo = LinkInformation.ClickerDeviceInfo.builder()
+                .browser(browser)
+                .device(device)
+                .operatingSystem(os)
+                .build();
+
+        LinkInformation linkInformation = LinkInformation.builder()
+                .associatedShortHash(hash)
+                .entityIpInformation(ipInfo)
+                .timeZone(linkAnalysisDto.getTrackPayloadDto().getTimezone())
+                .timeOfClick(Instant.now())
+                .deviceInfo(deviceInfo)
+                .build();
+
+        linkInformationService.save(linkInformation);
+
+    }
 
     private String getClientsIp(HttpServletRequest request) {
 
@@ -51,94 +123,33 @@ public class AnalysisService {
         return request.getRemoteAddr();
     }
 
-    public void sendDataToKafka(TrackPayloadDto dto, HttpServletRequest request) {
-
-        String ip = getClientsIp(request);
-
-        LinkAnalysisDto linkAnalysisDto = LinkAnalysisDto.builder()
-                .entityIp(ip)
-                .trackPayloadDto(dto)
-                .build();
-        kafkaTemplate.send("URL_SHORTENER_existed_link_analysis", linkAnalysisDto);
-    }
-
-    public void analyze(LinkAnalysisDto linkAnalysisDto) {
-        /*
-         */
-
-//        step1: check if shortHash exists or not, if not then return
-        String hash = linkAnalysisDto.getTrackPayloadDto().getShortHash();
-        Links linkInDb = linkService.getLinkByHash(hash);
-
-
-        if (hash == null || hash.isBlank() || linkInDb == null) return;
-
-
-//        step2: extract ip
-        String ip = linkAnalysisDto.getEntityIp();
-
-//        step3: get geoIpResponses for the extracted ip address
-        IpInfo ipInfo = getIpRelatedGeoInfoResponse(ip);
-
-        int width;
-        if (linkAnalysisDto.getTrackPayloadDto().getViewportWidth() == null
-                && linkAnalysisDto.getTrackPayloadDto().getScreenWidth() == null) {
-            width = 0;
-        } else {
-            width = linkAnalysisDto.getTrackPayloadDto().getViewportWidth() == null
-                    ? linkAnalysisDto.getTrackPayloadDto().getScreenWidth()
-                    : linkAnalysisDto.getTrackPayloadDto().getViewportWidth();
-        }
-
-//        step4: Browser categorization based on userAgent
-        Browser browser = categoriseBrowserBasedOnUserAgent(linkAnalysisDto.getTrackPayloadDto().getUserAgent());
-
-//        step5: OS categorization
-        OperatingSystem os = categoriseOperatingSystemBasedOnUserAgent(linkAnalysisDto.getTrackPayloadDto().getUserAgent());
-
-//        step6 :device categorization based on width, if device is non-mobile then -> device recategorization because resized window can result in wrong device conclusion
-        Device device = categoriseDeviceBasedOnWidthAndOperatingSystem(width, os);
-
-//        step7: increment short link click count and save
-        linkInDb.setClickCount(linkInDb.getClickCount() + 1);
-        linkService.save(linkInDb);
-
-//        step8:  create a POJO and save into db
-        LinkInformation.ClickerDeviceInfo deviceInfo = LinkInformation.ClickerDeviceInfo.builder()
-                .browser(browser)
-                .device(device)
-                .operatingSystem(os)
-                .build();
-
-        LinkInformation linkInformation = LinkInformation.builder()
-                .associatedShortHash(hash)
-                .entityIpInformation(ipInfo)
-                .timeZone(linkAnalysisDto.getTrackPayloadDto().getTimezone())
-                .timeOfClick(Instant.now())
-                .deviceInfo(deviceInfo)
-                .build();
-
-        linkInformationService.save(linkInformation);
-
-
-    }
-
     private OperatingSystem categoriseOperatingSystemBasedOnUserAgent(String userAgent) {
-        if (userAgent.contains("Windows")) return OperatingSystem.WINDOWS;
-        if (userAgent.contains("Mac")) return OperatingSystem.MAC;
-        if (userAgent.contains("Android")) return OperatingSystem.ANDROID;
-        if (userAgent.contains("iPhone")) return OperatingSystem.IOS;
-        if (userAgent.contains("Linux")) return OperatingSystem.LINUX;
-        else return OperatingSystem.OTHERS;
+        if (userAgent.contains("Windows"))
+            return OperatingSystem.WINDOWS;
+        if (userAgent.contains("Mac"))
+            return OperatingSystem.MAC;
+        if (userAgent.contains("Android"))
+            return OperatingSystem.ANDROID;
+        if (userAgent.contains("iPhone"))
+            return OperatingSystem.IOS;
+        if (userAgent.contains("Linux"))
+            return OperatingSystem.LINUX;
+        else
+            return OperatingSystem.OTHERS;
 
     }
 
     private Browser categoriseBrowserBasedOnUserAgent(String userAgent) {
-        if (userAgent.contains("Edg")) return Browser.MICROSOFT_EDGE;
-        if (userAgent.contains("Firefox")) return Browser.FIREFOX;
-        if (userAgent.contains("Chrome")) return Browser.CHROME;
-        if (userAgent.contains("Safari")) return Browser.SAFARI;
-        else return Browser.OTHERS;
+        if (userAgent.contains("Edg"))
+            return Browser.MICROSOFT_EDGE;
+        if (userAgent.contains("Firefox"))
+            return Browser.FIREFOX;
+        if (userAgent.contains("Chrome"))
+            return Browser.CHROME;
+        if (userAgent.contains("Safari"))
+            return Browser.SAFARI;
+        else
+            return Browser.OTHERS;
 
     }
 
@@ -158,7 +169,6 @@ public class AnalysisService {
                 }
                 return Device.OTHERS;
 
-
             case WINDOWS, MAC, LINUX:
                 if (width <= 500) {
                     // Resized desktop window
@@ -175,18 +185,21 @@ public class AnalysisService {
     }
 
     private IpInfo getIpRelatedGeoInfoResponse(String ip) {
-        String uriToCall = GENERIC_IP_INFO_API.replace("{ip}", ip);
         try {
-            return webClient.get()
-                    .uri(new URI(uriToCall))
-                    .retrieve()
-                    .bodyToMono(IpInfo.class)
-                    .block();
-        } catch (URISyntaxException e) {
-            log.error("Something went wrong while converting the api for GeoIp response. Exception: {}", e.getMessage());
-            return null;
-        } catch (Exception ex) {
-            log.error("Something went wrong while fetching GeoIp response. Exception: {}", ex.getMessage());
+            IPResponseLite details = client.lookupIP(ip);
+
+            IpInfo response = new IpInfo();
+
+            response.setIp(details.getIp());
+            response.setCountryCode(details.getCountryCode());
+            response.setCountry(details.getCountry());
+            response.setContinentCode(details.getContinentCode());
+            response.setContinent(details.getContinent());
+
+            return response;
+
+        } catch (RateLimitedException e) {
+            log.error("Rate Limited For IpInfoLite lookup, ip {}", ip);
             return null;
         }
     }
