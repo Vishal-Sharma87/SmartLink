23ed01675fc45e997b7c033241462dff5c23b140
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 21:27:11 +0530

    refactor: update RedirectServiceResponseDto to use Verdict enum instead of FinalVerdict

--- src/main/java/com/spring/springboot/UrlShortener/dto/RedirectServiceResponseDto.java
+++ src/main/java/com/spring/springboot/UrlShortener/dto/RedirectServiceResponseDto.java
@@ -1,15 +1,15 @@
 package com.spring.springboot.UrlShortener.dto;
 
 
-import com.spring.springboot.UrlShortener.enums.FinalVerdict;
 
+import com.spring.springboot.UrlShortener.enums.Verdict;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class RedirectServiceResponseDto {
-    private FinalVerdict.Verdict status;
+    private Verdict status;
     private String longUrl;
     private String shortHash;
 }
