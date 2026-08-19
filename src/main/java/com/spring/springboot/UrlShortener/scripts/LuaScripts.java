db49192b54e18cbe542a6b6fbc56710302a75d36
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 18:27:42 +0530

    feat: add LuaScripts class with OTP validation script

---
+++ src/main/java/com/spring/springboot/UrlShortener/scripts/LuaScripts.java
@@ -0,0 +1,22 @@
+package com.spring.springboot.UrlShortener.scripts;
+
+import org.springframework.data.redis.core.script.RedisScript;
+
+public class LuaScripts {
+
+    private static final String VALIDATE_OTP_SCRIPT_CONTENT = """
+                local key = KEYS[1]
+                local passedOtp = ARGV[1]
+                
+                local storedHashedOtp = redis.call('GET', key)
+                if storedHashedOtp == false or storedHashedOtp ~= passedOtp then
+                    return 0
+                end
+                redis.call('DEL', key)
+                return 1
+                """;
+
+    public static final RedisScript<Long> VALIDATE_OTP_SCRIPT = RedisScript.of(VALIDATE_OTP_SCRIPT_CONTENT, Long.class);
+
+
+}
