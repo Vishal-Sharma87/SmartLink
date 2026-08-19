2281269eb6c13b4f775fa59fb56cb3a0d64dfc62
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 18:29:35 +0530

    feat: remove unused import for Links entity in LinkInformationRepository

--- src/main/java/com/spring/springboot/UrlShortener/repositories/LinkInformationRepository.java
+++ src/main/java/com/spring/springboot/UrlShortener/repositories/LinkInformationRepository.java
@@ -1,7 +1,6 @@
 package com.spring.springboot.UrlShortener.repositories;
 
 import com.spring.springboot.UrlShortener.entity.LinkInformation;
-import com.spring.springboot.UrlShortener.entity.Links;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
