df2d079161832fdcb74826cdd71d13b1e20a27c9
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 19:01:14 +0530

    feat: add support for unique reports so that, system could discard duplicate reports from same EMAIL

--- src/main/java/com/spring/springboot/UrlShortener/controllers/ReportLinkController.java
+++ src/main/java/com/spring/springboot/UrlShortener/controllers/ReportLinkController.java
@@ -2,11 +2,6 @@ package com.spring.springboot.UrlShortener.controllers;
 
 import com.spring.springboot.UrlShortener.dto.requestDtos.ReportLinkRequestDto;
 import com.spring.springboot.UrlShortener.services.ReportLinkService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,37 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/report-abuse")
 @RequiredArgsConstructor
-@Tag(
-        name = "Report Abuse",
-        description = "Community-driven abuse reporting APIs for short links"
-)
 public class ReportLinkController {
 
     private final ReportLinkService reportLinkService;
 
 
 
-    @Operation(
-            summary = "Report a suspicious or abusive short link",
-            description = """
-            Allows an end user to report a shortened URL that appears unsafe,
-            misleading, or malicious.
-
-            OTP verification is required to confirm reporter identity.
-            Duplicate reports for the same link by the same reporter
-            are not allowed until verification is completed.
-            """
-    )
-    @ApiResponse(
-            responseCode = "200",
-            description = "Report accepted successfully",
-            content = @Content(
-                    mediaType = "text/plain",
-                    examples = @ExampleObject(
-                            value = "We have accepted your request, we'll notify you once we have confirmation about the link"
-                    )
-            )
-    )
     @PostMapping()
     public ResponseEntity<String> reportLink(@Valid @RequestBody ReportLinkRequestDto dto) {
 
