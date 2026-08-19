e32ff3bafd645001c1f7f5b65c5cdc9bfcadaf25
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 18:52:18 +0530

    feat: add regex for shortCode in decode method with invalid char handling

--- src/main/java/com/spring/springboot/UrlShortener/services/Base62.java
+++ src/main/java/com/spring/springboot/UrlShortener/services/Base62.java
@@ -1,15 +1,21 @@
 package com.spring.springboot.UrlShortener.services;
 
+import java.util.regex.Pattern;
+
+import com.spring.springboot.UrlShortener.advices.exceptions.NoSuchLinkExists;
 
 public class Base62 {
 
     private static final String CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    private static final Pattern HASH_PATTERN = Pattern.compile("^[0-9a-zA-Z]+$");
+
     private Base62() {
     }
 
     public static String encode(Long num) {
-        if (0 == num) return "0";
+        if (0 == num)
+            return "0";
 
         StringBuilder generatedHash = new StringBuilder();
         while (num > 0) {
@@ -21,10 +27,17 @@ public class Base62 {
     }
 
     public static Long decode(String hash) {
+        if (hash == null || !HASH_PATTERN.matcher(hash).matches()) {
+            throw new NoSuchLinkExists("Invalid shortcode: " + hash);
+        }
+        
         long res = 0L;
 
         for (int i = 0; i < hash.length(); i++) {
             int ind = CHARS.indexOf(hash.charAt(i));
+            if (ind < 0) {
+                throw new NoSuchLinkExists("Invalid shortcode: " + hash);
+            }
             res = res * 62 + ind;
         }
         return res;
