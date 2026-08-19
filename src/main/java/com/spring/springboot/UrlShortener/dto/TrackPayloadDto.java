b185ef198a88b980b51b068faef942761309a686
Vishal Sharma <vishalimp03@gmail.com>
2026-08-19 17:11:17 +0530

    refactor: remove Swagger annotations from DTOs for cleaner code

--- src/main/java/com/spring/springboot/UrlShortener/dto/TrackPayloadDto.java
+++ src/main/java/com/spring/springboot/UrlShortener/dto/TrackPayloadDto.java
@@ -1,7 +1,6 @@
 package com.spring.springboot.UrlShortener.dto;
 
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,42 +11,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
-@Schema(
-        description = "Payload sent from client browser to capture click analytics"
-)
 public class TrackPayloadDto {
     // used to get clicker's device's, browser's, etc. info for real time analysis
 
 
-    @Schema(
-            description = "Short hash identifying the clicked URL",
-            example = "aZ91Qe",
-            requiredMode = Schema.RequiredMode.REQUIRED
-    )
     private String shortHash;
 
-    @Schema(
-            description = "Device screen width used to classify device type",
-            example = "1920"
-    )
     private Integer screenWidth;
 
-    @Schema(
-            description = "Browser viewport width",
-            example = "1200"
-    )
     private Integer viewportWidth;
 
-    @Schema(
-            description = "Browser user-agent string",
-            example = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-    )
     private String userAgent;
 
-    @Schema(
-            description = "User's local timezone",
-            example = "Asia/Kolkata"
-    )
     private String timezone;
 }
-
