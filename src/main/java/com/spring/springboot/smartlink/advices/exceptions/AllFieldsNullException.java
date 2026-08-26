eb0d2fedd6e9975ca337e8d2c0e2a70997b1a0fb
Vishal Sharma <vishalimp03@gmail.com>
2026-08-26 16:53:34 +0530

    feat: add AllFieldsNullException class for handling null field scenarios, thrown when user passes all null in UserCredentials update

---
+++ src/main/java/com/spring/springboot/smartlink/advices/exceptions/AllFieldsNullException.java
@@ -0,0 +1,7 @@
+package com.spring.springboot.smartlink.advices.exceptions;
+
+public class AllFieldsNullException extends RuntimeException {
+    public AllFieldsNullException(String msg) {
+        super(msg);
+    }
+}
