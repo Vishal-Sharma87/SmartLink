d5cdc1f2a253b74518428adb9218f6c4ccad7964
Vishal Sharma <vishalimp03@gmail.com>
2026-08-26 16:34:26 +0530

    feat: add LinkAnalyticsResponseDto class for handling link analytics data with summary and click details

---
+++ src/main/java/com/spring/springboot/smartlink/dto/responseDtos/LinkAnalyticsResponseDto.java
@@ -0,0 +1,51 @@
+package com.spring.springboot.smartlink.dto.responseDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkAnalyticsResponseDto {
+    private String shortHash;
+    private String actualUrl;
+    private Integer totalClicks;
+    private Integer uniqueCountries;
+    private SummaryItem topCountry;
+    private List<SummaryItem> countries;
+    private List<SummaryItem> continents;
+    private List<SummaryItem> devices;
+    private List<SummaryItem> browsers;
+    private List<SummaryItem> operatingSystems;
+    private List<ClickItem> recentClicks;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SummaryItem {
+        private String label;
+        private long count;
+        private double percentage;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClickItem {
+        private Instant time;
+        private String country;
+        private String continent;
+        private String device;
+        private String browser;
+        private String operatingSystem;
+        private String timeZone;
+    }
+}
