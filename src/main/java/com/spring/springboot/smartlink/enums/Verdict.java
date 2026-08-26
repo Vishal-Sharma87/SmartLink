a323d018265bc776acec811a6bb0b80f8abb615a
Vishal Sharma <vishalimp03@gmail.com>
2026-08-26 16:18:43 +0530

    feat: add Verdict PENDING_REVERIFICATION enum, used when a reportCount of a link crosses threashold of 3, enabling grey zone redirection

---
+++ src/main/java/com/spring/springboot/smartlink/enums/Verdict.java
@@ -0,0 +1,14 @@
+package com.spring.springboot.smartlink.enums;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.ToString;
+
+@ToString
+public enum Verdict {
+    SAFE, SUSPICIOUS, UNVERIFIED, MALICIOUS, PENDING_REVERIFICATION;
+
+    @JsonValue
+    public String toJson() {
+        return this.name();
+    }
+}
