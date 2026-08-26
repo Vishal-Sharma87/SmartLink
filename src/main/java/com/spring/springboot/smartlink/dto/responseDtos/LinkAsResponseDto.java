14884d72879833166826a0846265b0b889062924
Vishal Sharma <vishalimp03@gmail.com>
2026-08-26 16:35:01 +0530

    feat: add LinkAsResponseDto class for link response handling with relevant fields

---
+++ src/main/java/com/spring/springboot/smartlink/dto/responseDtos/LinkAsResponseDto.java
@@ -0,0 +1,33 @@
+package com.spring.springboot.smartlink.dto.responseDtos;
+
+import com.spring.springboot.smartlink.enums.Verdict;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LinkAsResponseDto {
+
+    /*
+     * actual
+     * hash
+     * creation time
+     * status
+     * report cnt
+     * click cnt
+     * */
+    private String id;
+    private String actualUrl;
+    private String hashedKey;
+    private Verdict status;
+    private Integer clickCnt;
+    private Integer reportCnt;
+    private Instant creationTime;
+}
