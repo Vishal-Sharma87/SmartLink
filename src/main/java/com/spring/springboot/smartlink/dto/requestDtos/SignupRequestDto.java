f50a42f67081997897127f60e16672ea977d02dc
Vishal Sharma <vishalimp03@gmail.com>
2026-08-26 16:33:05 +0530

    feat: add SignupRequestDto class for user signup with validation constraints

---
+++ src/main/java/com/spring/springboot/smartlink/dto/requestDtos/SignupRequestDto.java
@@ -0,0 +1,23 @@
+package com.spring.springboot.smartlink.dto.requestDtos;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+
+@Data
+public class SignupRequestDto {
+
+    @NotBlank
+    private String userName;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String otp;
+}
