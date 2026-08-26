36710603257011fdbe053b69251679e89a4ba5de
Vishal Sharma <vishalimp03@gmail.com>
2026-08-26 16:41:47 +0530

    refactor: add @NotNull and @NotBlank validation on actualUrl

---
+++ src/main/java/com/spring/springboot/smartlink/dto/UrlToShortRequestDto.java
@@ -0,0 +1,13 @@
+package com.spring.springboot.smartlink.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class UrlToShortRequestDto {
+
+    @NotNull(message = "URL cannot be null")
+    @NotBlank(message = "URL cannot be blank")
+    private String actualUrl;
+}
