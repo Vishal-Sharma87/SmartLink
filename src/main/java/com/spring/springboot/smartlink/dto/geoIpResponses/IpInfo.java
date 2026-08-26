706aaec003dad989118e8526562cf729331d71a1
Vishal Sharma <vishalimp03@gmail.com>
2026-08-26 16:28:07 +0530

    feat: add IpInfo class for encapsulating geographical IP information including IP address, country code, and continent details

---
+++ src/main/java/com/spring/springboot/smartlink/dto/geoIpResponses/IpInfo.java
@@ -0,0 +1,23 @@
+package com.spring.springboot.smartlink.dto.geoIpResponses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class IpInfo {
+
+    @JsonProperty("ip")
+    private String ip;
+
+    @JsonProperty("country_code")
+    private String countryCode;
+
+    @JsonProperty("country")
+    private String country;
+
+    @JsonProperty("continent_code")
+    private String continentCode;
+
+    @JsonProperty("continent")
+    private String continent;
+}
