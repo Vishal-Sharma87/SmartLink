f6f6e43603b2b53bb264a0e9b7d58d82a43296cc
Vishal Sharma <vishalimp03@gmail.com>
2026-08-28 02:00:15 +0530

    feat: initialize SmartLinkApplication with Spring Boot setup

---
+++ src/main/java/com/spring/springboot/smartlink/SmartLinkApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springboot.smartlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLinkApplication.class, args);
+    }
+
+}
