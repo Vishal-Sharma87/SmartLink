00b9aba879f9ac5dacc84be4d07a92532ff2fd92
Vishal Sharma <vishalimp03@gmail.com>
2026-08-28 21:06:04 +0530

    feat: add RedirectionHashInvalidException for handling invalid redirection hash errors, thrown when decoded hash value is greater than the last url counter which sytem used

---
+++ src/main/java/com/spring/springboot/smartlink/advices/exceptions/RedirectionHashInvalidException.java
@@ -0,0 +1,9 @@
+package com.spring.springboot.smartlink.advices.exceptions;
+
+public class RedirectionHashInvalidException extends RuntimeException{
+
+    public RedirectionHashInvalidException(String message){
+        super(message);
+    }
+
+}
