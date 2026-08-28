327409cc01d72b531b5ed0b5cc4ce263644054e3
Vishal Sharma <vishalimp03@gmail.com>
2026-08-28 21:07:38 +0530

    feat: add RedirectionController to handle URL redirection from hash

---
+++ src/main/java/com/spring/springboot/smartlink/controllers/RedirectionController.java
@@ -0,0 +1,28 @@
+package com.spring.springboot.smartlink.controllers;
+import com.spring.springboot.smartlink.services.RedirectService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller // using @Controller because it supports html page rendering and user clicks
+@RequestMapping()
+@RequiredArgsConstructor
+public class RedirectionController {
+
+    private final RedirectService redirectService;
+
+    @GetMapping("/{hash}")
+    public String redirectionFromHashToLongUrl(@PathVariable String hash, Model model) {
+
+        return redirectService.putModelAttributesIfLinkExists(hash, model);
+
+    }
+
+
+}
