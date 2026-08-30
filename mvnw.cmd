460c0224a190fb9faffe256dedfaf559b35f3679
Vishal Sharma <vishalimp03@gmail.com>
2026-08-31 00:39:48 +0530

    chore: improve formatting and clarity of environment variable documentation in Maven Wrapper script

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,19 +1,19 @@
 <# : batch portion
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
-@REM or more contributor license agreements.  See the NOTICE file
+@REM or more contributor license agreements. See the NOTICE file
 @REM distributed with this work for additional information
-@REM regarding copyright ownership.  The ASF licenses this file
+@REM regarding copyright ownership. The ASF licenses this file
 @REM to you under the Apache License, Version 2.0 (the
 @REM "License"); you may not use this file except in compliance
-@REM with the License.  You may obtain a copy of the License at
+@REM with the License. You may obtain a copy of the License at
 @REM
 @REM    http://www.apache.org/licenses/LICENSE-2.0
 @REM
 @REM Unless required by applicable law or agreed to in writing,
 @REM software distributed under the License is distributed on an
 @REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied.  See the License for the
+@REM KIND, either express or implied. See the License for the
 @REM specific language governing permissions and limitations
 @REM under the License.
 @REM ----------------------------------------------------------------------------
@@ -21,10 +21,13 @@
 @REM ----------------------------------------------------------------------------
 @REM Apache Maven Wrapper startup batch script, version 3.3.4
 @REM
-@REM Optional ENV vars
-@REM   MVNW_REPOURL - repo url base for downloading maven distribution
-@REM   MVNW_USERNAME/MVNW_PASSWORD - user and password for downloading maven
-@REM   MVNW_VERBOSE - true: enable verbose log; others: silence the output
+@REM Environment variables:
+@REM   MVNW_REPOURL             - Base URL for downloading Maven.
+@REM   MVNW_USERNAME            - Username for Maven download authentication.
+@REM   MVNW_PASSWORD            - Password for Maven download authentication.
+@REM   MVNW_VERBOSE             - Controls output verbosity:
+@REM                              true - Enable verbose logging.
+@REM                              other values - Suppress output.
 @REM ----------------------------------------------------------------------------
 
 @IF "%__MVNW_ARG0_NAME__%"=="" (SET __MVNW_ARG0_NAME__=%~nx0)
