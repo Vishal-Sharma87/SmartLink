b920e7a2d0171d94541ebe1ce216a7fd1382d18a
Vishal Sharma <vishalimp03@gmail.com>
2026-08-30 22:45:51 +0530

    feat: add EmailResponse class for handling email response data

---
+++ src/main/java/com/spring/springboot/smartlink/email/dto/EmailResponse.java
@@ -0,0 +1,12 @@
+package com.spring.springboot.smartlink.email.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EmailResponse {
+    private String messageId;
+}
