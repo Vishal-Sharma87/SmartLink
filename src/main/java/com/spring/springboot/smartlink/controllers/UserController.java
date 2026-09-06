2f374115fa22e28bfd90fb52f4d7451df488f8c0
Vishal Sharma <vishalimp03@gmail.com>
2026-09-06 21:32:15 +0530

    refactor(user): remove credential-update endpoint pending redesign
    Remove the credential-update route and request DTO because the existing API design and implementation need a future redesign. This is an intentional API removal, not a side effect of package reorganization.

--- src/main/java/com/spring/springboot/smartlink/controllers/UserController.java
+++ src/main/java/com/spring/springboot/smartlink/controllers/UserController.java
@@ -1,10 +1,8 @@
 package com.spring.springboot.smartlink.controllers;
 
-import com.spring.springboot.smartlink.dto.UserCredentialUpdateRequestDto;
 import com.spring.springboot.smartlink.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -20,22 +18,6 @@ public class UserController {
     @GetMapping({"", "/"})
     public ResponseEntity<String> health(){
         return ResponseEntity.ok("Working");
-    }
-
-
-    @PutMapping("/update-user-credentials")
-    public ResponseEntity<Void> updateUserCredentials(
-            @RequestBody UserCredentialUpdateRequestDto user
-    ) {
-        try {
-            String userName =
-                    SecurityContextHolder.getContext().getAuthentication().getName();
-            userService.updateCredentials(user, userName);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("Failed to update credentials for user {}", SecurityContextHolder.getContext().getAuthentication().getName(), e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
     }
 
     @DeleteMapping("/delete-user")
