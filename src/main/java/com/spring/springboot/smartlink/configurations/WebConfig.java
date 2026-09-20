18b8c425a9c6ac5b7a895b495630cbe84585c245
Vishal Sharma <vishalimp03@gmail.com>
2026-09-20 20:26:26 +0530

    chore(http): configure Reactor Netty WebClient connector

--- src/main/java/com/spring/springboot/smartlink/configurations/WebConfig.java
+++ src/main/java/com/spring/springboot/smartlink/configurations/WebConfig.java
@@ -2,14 +2,20 @@ package com.spring.springboot.smartlink.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        HttpClient httpClient = HttpClient.newConnection();
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 
 }
